05113d7f8a8dec0b9b1da92c18c612a69fdb68f9
Ralph Greaves <rgreaves@ycp.edu>
2023-04-26 12:06:48 -0400

    Update event_applications.permissions.mjs

--- src/event_applications/event_applications.permissions.mjs
+++ src/event_applications/event_applications.permissions.mjs
@@ -2,6 +2,10 @@ export const GET_EVENT_APPLICATION_PERMISSIONS = [
   'read:event_application'
 ];
 
+export const GET_EVENT_APPLICATIONS_SUMMARY_PERMISSIONS = [
+  'read:event_applications_summary'
+];
+
 export const LIST_EVENT_APPLICATIONS_PERMISSIONS = [
   'list:event_applications'
 ];
