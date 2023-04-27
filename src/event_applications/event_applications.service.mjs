4de5f5016873690df4a3bf939d24ea94ec099997
Ralph Greaves <rgreaves@ycp.edu>
2023-04-26 20:16:02 -0400

    Update event_applications.service.mjs

--- src/event_applications/event_applications.service.mjs
+++ src/event_applications/event_applications.service.mjs
@@ -2,6 +2,55 @@ import mysqlx from '@mysql/xdevapi';
 
 const config = process.env.MYSQLX_CONFIG;
 
+export async function createEventApplication(event_application) {
+  const {
+    uid,
+    age,
+    country,
+    school,
+    edu_lvl,
+    edu_completed,
+    underrep_group,
+    gender,
+    pronouns,
+    race,
+    sexual_orient,
+    major,
+    t_size,
+    food_restrict,
+    github,
+    linked_in
+  } = event_application;
+  
+  const session = await mysqlx.getSession(config);
+
+  const response = await session
+      .sql('CALL create_event_application(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);')
+      .bind(uid,
+            age, 
+            country,
+            school,
+            edu_lvl,
+            edu_completed,
+            underrep_group,
+            gender,
+            pronouns,
+            race,
+            sexual_orient,
+            major,
+            t_size,
+            food_restrict,
+            github,
+            linked_in)
+      .execute();
+
+  const data = await response.fetchAll();
+
+  session.close();
+
+  return { data };
+}
+
 export async function getEventApplication(event_application_id) {
   const session = await mysqlx.getSession(config);
 
