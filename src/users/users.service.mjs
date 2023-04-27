a25fb0bda1a220764f4dfdb09255a0462cf0b300
Ralph Greaves <rgreaves@ycp.edu>
2023-04-26 21:56:14 -0400

    Update users.service.mjs

--- src/users/users.service.mjs
+++ src/users/users.service.mjs
@@ -3,11 +3,11 @@ import mysqlx from '@mysql/xdevapi';
 const config = process.env.MYSQLX_CONFIG;
 
 export async function createUser(user) {
-  const sub = user.sub;
+  const { sub } = user;
 
   const session = await mysqlx.getSession(config);
 
-  const response = await sesssion
+  const response = await session
       .sql('CALL create_user(?);')
       .bind(sub)
       .execute();
